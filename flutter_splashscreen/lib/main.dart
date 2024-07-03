8568d25dffe53cd3ba518ad2638a750807dab4fb
LeeSomgyul <140570847+LeeSomgyul@users.noreply.github.com>
2024-07-03 12:25:20 +0900

    전자액자
    자동으로 넘어가는 사진 구현
    (상단 광고가 자동으로 넘어가는 것)

--- flutter_splashscreen/lib/main.dart
+++ flutter_splashscreen/lib/main.dart
@@ -1,13 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_splashscreen/screen/home_screen.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(
-    MaterialApp(
-      home: HomeScreen(),
-    ),
-  );
+  runApp(const MaterialApp(
+    home: HomeScreen(),
+  ));
 }
--- flutter_splashscreen/lib/screen/home_screen.dart
+++ flutter_splashscreen/lib/screen/home_screen.dart
@@ -1,35 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:async';
 
-final homeUrl = Uri.parse('https://blog.codefactory.ai');
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
-class HomeScreen extends StatelessWidget {
-  WebViewController controller = WebViewController()
-    ..loadRequest(homeUrl)
-    ..setJavaScriptMode(JavaScriptMode.unrestricted);
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
-  HomeScreen({super.key});
+class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController controller = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(
+      const Duration(seconds: 2),
+      (timer) {
+        int currnetPage = controller.page!.toInt();
+        int nextPage = currnetPage + 1;
+
+        if (nextPage > 2) {
+          nextPage = 0;
+        }
+
+        controller.animateToPage(nextPage,
+            duration: const Duration(milliseconds: 500), curve: Curves.linear);
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    if (timer != null) {
+      timer!.cancel();
+    }
+
+    controller.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.yellow[700],
-        title: const Text('솜귤'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              controller.loadRequest(homeUrl);
-            },
-            icon: const Icon(
-              Icons.home,
-            ),
-          ),
-        ],
-      ),
-      body: WebViewWidget(
+      body: PageView(
         controller: controller,
+        children: [1, 2, 3]
+            .map((e) => Image.asset(
+                  'asset/img/짱구$e.png',
+                  fit: BoxFit.cover,
+                ))
+            .toList(),
       ),
     );
   }
