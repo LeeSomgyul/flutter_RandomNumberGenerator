fd4494bc0ed1b2b7e19a46eef55993931d065404
LeeSomgyul <140570847+LeeSomgyul@users.noreply.github.com>
2024-07-03 14:13:31 +0900

    우리 사귄지 몇일
    font를 야멜에 입력

--- flutter_splashscreen/lib/screen/home_screen.dart
+++ flutter_splashscreen/lib/screen/home_screen.dart
@@ -1,60 +1,10 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  Timer? timer;
-  PageController controller = PageController();
-
-  @override
-  void initState() {
-    super.initState();
-
-    timer = Timer.periodic(
-      const Duration(seconds: 2),
-      (timer) {
-        int currnetPage = controller.page!.toInt();
-        int nextPage = currnetPage + 1;
-
-        if (nextPage > 2) {
-          nextPage = 0;
-        }
-
-        controller.animateToPage(nextPage,
-            duration: const Duration(milliseconds: 500), curve: Curves.linear);
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    if (timer != null) {
-      timer!.cancel();
-    }
-
-    controller.dispose();
-
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        controller: controller,
-        children: [1, 2, 3]
-            .map((e) => Image.asset(
-                  'asset/img/짱구$e.png',
-                  fit: BoxFit.cover,
-                ))
-            .toList(),
-      ),
-    );
+    return const Placeholder();
   }
 }
