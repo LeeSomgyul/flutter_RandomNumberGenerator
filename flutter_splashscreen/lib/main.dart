cadeb7e2b39d8ff91d6845e93c6471a3638501a3
LeeSomgyul <140570847+LeeSomgyul@users.noreply.github.com>
2024-07-04 16:21:21 +0900

    우리 사귄지 몇일
    레이아웃 작업, 디자인, 테마 사용, DatePicker사용, d-day구현

--- flutter_splashscreen/lib/main.dart
+++ flutter_splashscreen/lib/main.dart
@@ -3,7 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_splashscreen/screen/home_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: HomeScreen(),
+  runApp(MaterialApp(
+    theme: ThemeData(
+      fontFamily: '온글잎 언즈체',
+      textTheme: const TextTheme(
+        displayLarge: TextStyle(
+          color: Colors.white,
+          fontSize: 80,
+          fontFamily: 'cafe24',
+        ),
+        displayMedium: TextStyle(
+          color: Colors.white,
+          fontSize: 50,
+        ),
+        displaySmall: TextStyle(
+          color: Colors.white,
+          fontSize: 30,
+        ),
+      ),
+    ),
+    home: const HomeScreen(),
   ));
 }
--- flutter_splashscreen/lib/screen/home_screen.dart
+++ flutter_splashscreen/lib/screen/home_screen.dart
@@ -1,10 +1,115 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        bottom: false,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: const Column(
+            children: [
+              _top(),
+              _bottom(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _top extends StatefulWidget {
+  const _top({super.key});
+
+  @override
+  State<_top> createState() => _topState();
+}
+
+class _topState extends State<_top> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    final now = DateTime.now();
+
+    return Expanded(
+      child: Container(
+        child: Column(
+          children: [
+            Text(
+              'U&I',
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            Text(
+              '우리 처음 만난날',
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+            Text(
+              '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+            IconButton(
+              iconSize: 30,
+              color: Colors.red,
+              onPressed: () {
+                showCupertinoDialog(
+                  barrierDismissible: true,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        color: Colors.white,
+                        height: 300,
+                        child: CupertinoDatePicker(
+                          initialDateTime: selectedDate,
+                          mode: CupertinoDatePickerMode.date,
+                          onDateTimeChanged: (DateTime date) {
+                            setState(() {
+                              selectedDate = date;
+                            });
+                          },
+                          dateOrder: DatePickerDateOrder.ymd,
+                          maximumDate: DateTime.now(),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              icon: const Icon(
+                Icons.favorite,
+              ),
+            ),
+            Text(
+              'D+${now.difference(selectedDate).inDays + 1}',
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _bottom extends StatelessWidget {
+  const _bottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Image.asset(
+          'asset/img/배경화면.png',
+        ),
+      ),
+    );
   }
 }
